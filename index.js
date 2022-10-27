2c9b490e06435ab8d69255e8cda66ca3b4215a38
Àfrica Calderón Alcaraz <africa@imac-de-africa.home>
2022-10-27 19:33:18 +0200

    1

---
+++ index.js
@@ -0,0 +1,20 @@
+// Iteration 1: Names and Input
+
+const hacker1 = "John"
+console.log (`The driver's name is ${hacker1}`)
+
+const hacker2 = "Pepe"
+console.log(`The navigator's name is ${hacker2}`)
+
+// Iteration 2: Conditionals
+
+if (hacker1.length > hacker2.length) {
+    console.log (`"The driver has the longest name, it has ${hacker1.length} characters."`)
+} else if (hacker1.length < hacker2.length) {
+    console.log (`"It seems that the navigator has the longest name, it has ${hacker1.length} characters."`)
+} else if (hacker1.length = hacker2.length) {
+    console.log (`"Wow, you both have equally long names, ${hacker1.length} characters!"`)
+}
+
+// Iteration 3: Loops
+
